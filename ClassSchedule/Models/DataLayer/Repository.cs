f8266883f3afc4d3dcff5ee07d3e56f5e4c3c124
firaol81 <firaol.baneta@southeasttech.edu>
2024-03-17 22:29:21 -0500

    Yes

--- ClassSchedule/Models/DataLayer/Repository.cs
+++ ClassSchedule/Models/DataLayer/Repository.cs
@@ -18,7 +18,8 @@ namespace ClassSchedule.Models
         public virtual IEnumerable<T> List(QueryOptions<T> options)
         {
             IQueryable<T> query = dbset;
-            foreach (string include in options.GetIncludes()) {
+            foreach (string include in options.GetIncludes())
+            {
                 query = query.Include(include);
             }
             if (options.HasWhere)
@@ -34,5 +35,4 @@ namespace ClassSchedule.Models
         public virtual void Delete(T entity) => dbset.Remove(entity);
         public virtual void Save() => context.SaveChanges();
     }
-
 }
